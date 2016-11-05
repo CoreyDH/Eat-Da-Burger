556df76d59888b5297c053ac48f8e6b1ca73955b
Corey H <rewer6@gmail.com>
2016-11-04 23:18:09 -0500

    Updating port for heroku

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/controller.js');
 app.use('/', routes);
 
-app.listen(3000, function() {
-	console.log('Listening on port 3000');
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+	console.log('Listening on port '+PORT);
 });
